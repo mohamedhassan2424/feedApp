6367fca3b7525d683e4f1d6d2eea6be9a4644048
MohamedHassan <hassanmohamed040@gmail.com>
2023-03-08 11:12:26 -0500

    forgot to save for the userService section 7.6

--- src/main/java/com/bptn/feedapp/service/UserService.java
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -1,5 +1,30 @@
 package com.bptn.feedapp.service;
 
-public class UserService {
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.bptn.feedapp.jdbc.UserDao;
+
+import com.bptn.feedapp.jdbc.UserBean;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	UserDao userDao;
+	
+	public List<UserBean> listUsers() {
+		
+		return this.userDao.listUsers();
+	}
+	
+	public UserBean findByUsername(String username) {
+		return this.userDao.findByUsername(username);
+	}
+	
+	public void createUser(UserBean user) {
+		this.userDao.createUser(user);
+	}
 }
