e146823500063027ae0aeeb11d7c1612fda66e40
MohamedHassan <hassanmohamed040@gmail.com>
2023-03-20 13:38:15 -0400

    create a exception pacakage and than a domain package in that class with 8 classes created

---
+++ src/main/java/com/bptn/feedapp/exception/ExceptionHandling.java
@@ -0,0 +1,9 @@
+package com.bptn.feedapp.exception;
+
+public class ExceptionHandling {
+
+	public ExceptionHandling() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/com/bptn/feedapp/exception/domain/EmailExistException.java
@@ -0,0 +1,14 @@
+package com.bptn.feedapp.exception.domain;
+
+
+
+public class EmailExistException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public EmailExistException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
